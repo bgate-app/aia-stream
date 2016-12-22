9ddf97d62d9d7cf544618f9b85ff8d546ad66c69
bgate-app <bgate.app@gmail.com>
2016-12-22 14:53:07 +0700

    sada

--- src/android/AiaStream.java
+++ src/android/AiaStream.java
@@ -1,35 +1,110 @@
+
 package com.aia.stream;
+
+import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class AiaStream extends CordovaPlugin {
-
+    private String TAG = getClass().getSimpleName();
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("startStream")) {
-            this.startStream(callbackContext);
+        if (action.equals("init")) {
+            this.onInit();
+            return true;
+        }else if (action.equals("startCameraPreview")) {
+            this.onStartCameraPreview(args, callbackContext);
+            return true;
+        }else if (action.equals("stopCameraPreview")) {
+            this.onStopCameraPreview(args, callbackContext);
+            return true;
+        }else if (action.equals("startBroadcast")) {
+            this.onStartBroadcast(args, callbackContext);
+            return true;
+        }else if (action.equals("stopBroadcast")) {
+            this.onStopBroadcast(args, callbackContext);
+            return true;
+        }else if (action.equals("switchCamera")) {
+            this.onSwitchCamera(args, callbackContext);
+            return true;
+        }else if (action.equals("setVideoQuality")) {
+            this.onSetVideoQuality(args, callbackContext);
+            return true;
+        }else if (action.equals("setBroadcastConfig")) {
+            this.onSetBroadcastConfig(args, callbackContext);
+            return true;
+        }else if (action.equals("setAudioEnable")) {
+            this.onSetAudioEnable(args, callbackContext);
+            return true;
+        }else if (action.equals("isAudioEnable")) {
+            this.isAudioEnable(args, callbackContext);
+            return true;
+        }else if (action.equals("isBroadcasting")) {
+            this.isBroadcasting(args, callbackContext);
+            return true;
+        }else if (action.equals("getCameraDirection")) {
+            this.onGetCameraDirection(args, callbackContext);
             return true;
         }
-        if (action.equals("stopStream")) {
-            this.stopStream(callbackContext);
-            return true;
-        }
-      
         return false;
     }
+
+    private void onInit() {
+        Log.d(TAG, "onInit: ");
+    }
+
+    private void onSwitchCamera(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onSwitchCamera: "+args.toString());
+    }
+
+    private void onStopBroadcast(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onStopBroadcast: "+args.toString());
+    }
+
+    private void onSetVideoQuality(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onSetVideoQuality: "+ args.toString());
+    }
+
+    private void onSetBroadcastConfig(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onSetBroadcastConfig: "+args.toString());
+    }
+
+    private void onSetAudioEnable(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onSetAudioEnable: "+args.toString());
+    }
+
+    private void isAudioEnable(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "isAudioEnable: "+args.toString());
+    }
+
+    private void isBroadcasting(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "isBroadcasting: "+args.toString());
+    }
+
+    private void onGetCameraDirection(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onGetCameraDirection: "+args.toString());
+    }
+
+    private void onStartBroadcast(JSONArray args, CallbackContext callbackContext) {
+        Log.d(TAG, "onStartBroadcast: "+args.toString());
+    }
+
+    private void onStartCameraPreview(JSONArray args, CallbackContext callbackContext){
+        Log.d(TAG, "onStartCameraPreview: "+args.toString());
+    }
+    private void onStopCameraPreview(JSONArray args, CallbackContext callbackContext){
+        Log.d(TAG, "onStopCameraPreview: "+args.toString());
+    }
+
     private void startStream( CallbackContext callbackContext) {
-        
+        Log.d(TAG, "startStream: ");
     }
     private void stopStream( CallbackContext callbackContext) {
-        
+        Log.d(TAG, "stopStream: ");
     }
-    
+
 }
--- www/AiaStream.js
+++ www/AiaStream.js
@@ -4,15 +4,67 @@ var AiaStream = function () {
 
 }
 
-AiaStream.prototype.startStream = function (options) {
-	options = options || {};
-	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "startStream", [options]);
+AiaStream.prototype.init = function () {
+	exec(null, null, "AiaStream", "init", []);
 };
 
-AiaStream.prototype.stopStream = function (options) {
-	options = options || {};
-	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "stopStream", [options]);
+AiaStream.prototype.startCameraPreview = function () {
+	return new Promise((resolve,reject)=>{
+		exec(function(){resolve();}, function(){reject();}, "AiaStream", "startCameraPreview", []);
+	});	
 };
+
+AiaStream.prototype.stopCameraPreview = function (options) {
+	return new Promise((resolve,reject)=>{
+		exec(function(){resolve();}, function(){reject();}, "AiaStream", "stopCameraPreview", []);
+	});	
+};
+
+AiaStream.prototype.startBroadcast = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "startBroadcast", []);
+};
+
+AiaStream.prototype.stopBroadcast = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "stopBroadcast", []);
+};
+
+AiaStream.prototype.switchCamera = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "switchCamera", []);
+};
+
+AiaStream.prototype.setVideoQuality = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "setVideoQuality", [options]);
+};
+
+AiaStream.prototype.setBroadcastConfig = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "setBroadcastConfig", [options]);
+};
+
+AiaStream.prototype.setAudioEnable = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "setAudioEnable", [options]);
+};
+
+AiaStream.prototype.isAudioEnable = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "isAudioEnable", [options]);
+};
+
+AiaStream.prototype.isBroadcasting = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "isBroadcasting", [options]);
+};
+
+AiaStream.prototype.getCameraDirection = function (options) {
+	options = options || {};
+	exec(options.successCallback || null, options.errorCallback || null, "AiaStream", "getCameraDirection", [options]);
+};
+
 
 var aiaStream = new AiaStream();
 
